b4a5766a2856a3c9fb2b528724891859443a551b
Carl Hartry Jr <carlhartry+github@gmail.com>
2023-06-12 13:13:15 -0400

    changes

--- src/main/java/com/hartrytech/llc/GameDemo.java
+++ src/main/java/com/hartrytech/llc/GameDemo.java
@@ -1,5 +1,7 @@
 package com.hartrytech.llc;
+
 import com.hartrytech.llc.Game.Scripts.Board.Menus;
+
 
 /**
  * JavaFX App
@@ -10,7 +12,7 @@ public class GameDemo
     public static void main(String[] args) 
     {
         Menus game = new Menus();
-        game.start(args);
+        game.startGame(args);
     }
 
 }
