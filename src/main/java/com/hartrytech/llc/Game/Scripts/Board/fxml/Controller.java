4f6e5a96e8385ddadd357236a14102e6e5217a5b
Carl Hartry Jr <carlhartry+github@gmail.com>
2023-06-14 13:01:52 -0400

    added a Controller

---
+++ src/main/java/com/hartrytech/llc/Game/Scripts/Board/fxml/Controller.java
@@ -0,0 +1,19 @@
+package com.hartrytech.llc.Game.Scripts.Board.fxml;
+
+import javafx.event.ActionEvent;
+
+public class Controller 
+{
+    public void games(ActionEvent e)
+    {
+        System.out.println("games is working");
+    }
+    public void  credits(ActionEvent e)
+    {
+        System.out.println("credits is working");
+    }
+    public void settings(ActionEvent e)
+    {
+        System.out.println("settings is working");
+    }
+}
