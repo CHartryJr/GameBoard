2978929db716ce1c9cc8330e7e6ed806f83ad7a4
Carl Hartry Jr <carlhartry+github@gmail.com>
2023-06-14 14:47:23 -0400

    more fmxl files

--- src/main/java/com/hartrytech/llc/Game/Scripts/Board/Menus.java
+++ src/main/java/com/hartrytech/llc/Game/Scripts/Board/Menus.java
@@ -5,8 +5,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-
 import javafx.stage.Stage;
 
 public class Menus extends Application
@@ -29,6 +27,13 @@ public class Menus extends Application
     {
         Application.launch();
     }
-   
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Menus.class.getResource("fxml/"+ fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
 
 }
--- src/main/java/com/hartrytech/llc/Game/Scripts/Board/fxml/Controller.java
+++ src/main/java/com/hartrytech/llc/Game/Scripts/Board/fxml/Controller.java
@@ -1,19 +1,39 @@
 package com.hartrytech.llc.Game.Scripts.Board.fxml;
 
+import java.io.IOException;
+import com.hartrytech.llc.Game.Scripts.Board.Menus;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
+/**
+ * Is a class that will be used to control the scene behaviors
+ */
 public class Controller 
 {
+     @FXML
+     public void switchScene(String sceneName) 
+    {
+        try 
+        {
+            Menus.setRoot(sceneName);
+        } 
+        catch (IOException e) 
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+   
     public void games(ActionEvent e)
     {
-        System.out.println("games is working");
+        switchScene("Games");
     }
     public void  credits(ActionEvent e)
     {
-        System.out.println("credits is working");
+        switchScene("Credits");
     }
     public void settings(ActionEvent e)
     {
-        System.out.println("settings is working");
+        switchScene("Settings");
     }
 }
